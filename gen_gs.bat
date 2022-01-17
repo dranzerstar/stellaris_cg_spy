54cbeb7d9fd78a3a6e2d704b6b68e826c4ab30b3
anton <anton>
2022-01-17 17:38:52 +0800

    pant des all done

---
+++ gen_gs.bat
@@ -0,0 +1,11 @@
+﻿@echo off
+setlocal EnableDelayedExpansion
+
+for /L %%i in (2, 1, 21) do (
+     set "num=000000%%i"
+     set "num=!num:~-2!"
+rem     echo !num!
+echo CG_plus_thief_auction_screen_events.1001.desc.!num!:0 "拍照地点:[ptowner!num!.GetName]\n颜色:\n备注:"
+
+
+)
---
+++ gen_gs3.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+for /L %%i in (2, 1, 21) do (
+     set "num=000000%%i"
+     set "num=!num:~-2!"
+rem     echo !num!
+echo CG_plus_thief_auction_screen_events.1001.desc.!num!:0 "îèè∆ínì_:[ptowner!num!.GetName]\n?êF:\n?íç:"
+
+
+)
