3d83d30b3e60cd87eb1a85335f41965251a91f4a
dranzerstar <mssh@h>
2022-01-30 21:05:28 +0800

    maid

--- gen_gs.bat
+++ gen_gs.bat
@@ -1,11 +1,12 @@
-﻿@echo off
+@echo off
 setlocal EnableDelayedExpansion
 
-for /L %%i in (2, 1, 21) do (
+for /L %%i in (0, 1, 5) do (
      set "num=000000%%i"
      set "num=!num:~-2!"
 rem     echo !num!
-echo CG_plus_thief_auction_screen_events.1001.desc.!num!:0 "拍照地点:[ptowner!num!.GetName]\n颜色:\n备注:"
-
-
+echo cg_plus_uniform_evt_pic_18!num! = {
+echo 	potential = {FROM = { check_variable = { which = cg_plus_evt_pic value =18!num! }}}
+echo 	allow = { always = yes }
+echo 	effect = {} }
 )
