b5e3cb0c49056533d9051fe67fa127a1b900c847
dranzerstar <mssh@h>
2022-04-10 15:25:47 +0800

    imf_half

--- descriptor.mod
+++ descriptor.mod
@@ -1,8 +1,8 @@
-version="1.5"
+version="3.0.2"
 tags={
 	"Events"
 	"Diplomacy"
 }
-name="CG_plus"
+name="多彩银河+ 多彩事件拓展包"
 supported_version="3.3.*"
 remote_file_id="2677457692"
--- gen_gs.bat
+++ gen_gs.bat
@@ -1,12 +1,10 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-for /L %%i in (0, 1, 5) do (
+for /L %%i in (0, 1, 41) do (
      set "num=000000%%i"
      set "num=!num:~-2!"
 rem     echo !num!
-echo cg_plus_uniform_evt_pic_18!num! = {
-echo 	potential = {FROM = { check_variable = { which = cg_plus_evt_pic value =18!num! }}}
-echo 	allow = { always = yes }
-echo 	effect = {} }
+echo 1={  change_leader_portrait = ELF!num! }
+
 )
